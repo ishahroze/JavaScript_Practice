8547a1755499680e711b25278fe526adbd92fe65
Rao Shahroz Ali <141501953+ishahroze@users.noreply.github.com>
2024-07-08 13:56:25 +0500

    Java Script Practice

---
+++ password_generator/src/components/Password.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+import { useState } from 'react'
+function Password() {
+  return (
+    <div className="bg-black min-h-screen text-white ">Password</div>
+  )
+}
+
+export default Password
